76cc200903b1a2a365251761f7ef05d7b4342940
Prince Effiong <prynzwill147@gmail.com>
2024-07-01 13:29:25 +0100

    TransactionList

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,34 @@
   <div class="container">
     <Balance />
     <IncomeExpenses />
-    <TransactionList />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
 
-<script>
+<script setup>
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import AddTransaction from './components/AddTransaction.vue';
 import TransactionList from './components/TransactionList.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
+import { ref } from 'vue';
 
-export default {
-  components: {
-    Header,
-    Balance,
-    AddTransaction,
-    TransactionList,
-    IncomeExpenses,
+const transactions = ref([
+  {
+    id: 1,
+    text: 'Flower',
+    amount: -15.02,
   },
-};
+  {
+    id: 2,
+    text: 'Book',
+    amount: 130,
+  },
+  {
+    id: 3,
+    text: 'Food',
+    amount: -8,
+  },
+]);
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,7 +1,7 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    //CLASSES TO BE MADE DYNAMIC
+    <!-- CLASSES TO BE MADE DYNAMIC -->
     <!-- <li class="minus">
       Cash <span> -$400</span>
       <button class="delete-btn">X</button>
@@ -14,13 +14,13 @@
   </ul>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      //Transactions array should be empty by default
-      transactions: [],
-    };
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  transactions: {
+    type: array,
+    required: true,
   },
-};
+});
 </script>
